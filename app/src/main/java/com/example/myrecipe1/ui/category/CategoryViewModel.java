784b09bcd5ff763cbb67975bc2fe458968798b89
Muhammad Rizky Ramadhan <mhd.rizky0311@gmail.com>
2024-06-02 18:20:55 +0700

    update

---
+++ app/src/main/java/com/example/myrecipe1/ui/category/CategoryViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myrecipe1.ui.category;
+
+import androidx.lifecycle.ViewModel;
+
+public class CategoryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/myrecipe1/ui/profile/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myrecipe1.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
